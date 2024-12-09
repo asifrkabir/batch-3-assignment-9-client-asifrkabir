e5c5a147459f6f60639cf4b0963fa288f8f8a9f9
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-12-09 23:23:31 +0600

    Product Filtering added

--- src/components/product/Products.tsx
+++ src/components/product/Products.tsx
@@ -2,10 +2,17 @@
 
 import InfiniteScrollContainer from "@/components/Shared/InfiniteScrollContainer";
 import { useGetAllProductsForFeedInfinite } from "@/hooks/product.hook";
+import { IQueryParam } from "@/types";
+import { useState } from "react";
+import AllProductsFilter from "./AllProductsFilter";
 import ProductCard from "./ProductCard";
 import ProductCardLoadingSkeleton from "./ProductCardLoadingSkeleton";
 
 const Products = () => {
+  const [params, setParams] = useState<IQueryParam[]>([
+    { name: "limit", value: 9 },
+  ]);
+
   const {
     data,
     fetchNextPage,
@@ -13,51 +20,54 @@ const Products = () => {
     isFetchingNextPage,
     isLoading,
     isError,
-  } = useGetAllProductsForFeedInfinite([{ name: "limit", value: 9 }]);
+  } = useGetAllProductsForFeedInfinite(params);
 
   const products = data?.pages.flatMap((page) => page.data || []) || [];
 
-  if (isLoading) {
-    return (
-      <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-        {Array.from({ length: 6 }).map((_, idx) => (
-          <ProductCardLoadingSkeleton key={idx} />
-        ))}
-      </div>
-    );
-  }
-
-  if (isError) {
-    return <p>Something went wrong while fetching products.</p>;
-  }
-
-  if (products.length <= 0) {
-    return (
-      <div className="flex flex-col items-center gap-1 text-center my-40">
-        <h3 className="text-2xl font-bold tracking-tight">
-          No products are available right now
-        </h3>
-        <p className="text-sm text-muted-foreground">
-          Please check back later.
-        </p>
-      </div>
-    );
-  }
-
   return (
-    <InfiniteScrollContainer
-      className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
-      onBottomReached={() => hasNextPage && fetchNextPage()}
-    >
-      {products.map((product) => (
-        <ProductCard key={product._id} product={product} />
-      ))}
-      {isFetchingNextPage && (
-        <div className="col-span-full text-center">
-          <ProductCardLoadingSkeleton />
+    <div className="w-full">
+      <AllProductsFilter setParams={setParams} />
+
+      {isLoading ? (
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {Array.from({ length: 6 }).map((_, idx) => (
+            <ProductCardLoadingSkeleton key={idx} />
+          ))}
         </div>
+      ) : isError ? (
+        <p>Something went wrong while fetching products.</p>
+      ) : products.length === 0 ? (
+        <div className="flex flex-col items-center gap-1 text-center my-40">
+          <h3 className="text-2xl font-bold tracking-tight">
+            No products are available right now
+          </h3>
+          <p className="text-sm text-muted-foreground">
+            Please check back later.
+          </p>
+        </div>
+      ) : (
+        <>
+          {/* Infinite Scroll Container */}
+          <InfiniteScrollContainer
+            className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+            onBottomReached={() => hasNextPage && fetchNextPage()}
+          >
+            {products.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </InfiniteScrollContainer>
+
+          {/* Next Page Loading Skeleton */}
+          {isFetchingNextPage && (
+            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              {Array.from({ length: 3 }).map((_, idx) => (
+                <ProductCardLoadingSkeleton key={idx} />
+              ))}
+            </div>
+          )}
+        </>
       )}
-    </InfiniteScrollContainer>
+    </div>
   );
 };
 
