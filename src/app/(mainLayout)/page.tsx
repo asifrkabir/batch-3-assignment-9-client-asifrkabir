c71b4132e6e9cd5dc74e2e54b1fa89ac89547e40
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-12-10 01:41:37 +0600

    Banner created

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,8 +1,12 @@
 import Products from "@/components/product/Products";
+import Banner from "./components/Banner";
 
 export default function HomePage() {
   return (
     <div className="h-full flex-1 flex-col space-y-2 p-8 md:flex">
+      <div className="mb-20">
+        <Banner />
+      </div>
       <div className="flex items-center justify-between mb-8">
         <h1 className="text-lg font-semibold md:text-2xl">Products</h1>
       </div>
--- src/app/(mainLayout)/products/page.tsx
+++ src/app/(mainLayout)/products/page.tsx
@@ -1,7 +1,12 @@
+import Products from "@/components/product/Products";
+
 const ProductsPage = () => {
   return (
-    <div>
-      <h1>This is ProductsPage</h1>
+    <div className="h-full flex-1 flex-col space-y-2 p-8 md:flex">
+      <div className="flex items-center justify-between mb-8">
+        <h1 className="text-lg font-semibold md:text-2xl">All Products</h1>
+      </div>
+      <Products />
     </div>
   );
 };
