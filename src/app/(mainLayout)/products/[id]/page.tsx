0f8038b188f7bf2826383b325309f71f944fe46e
Asif Rezwan Kabir <asifrkabir20@gmail.com>
2024-12-10 22:22:00 +0600

    View Reviews implemented

--- src/app/(mainLayout)/products/[id]/page.tsx
+++ src/app/(mainLayout)/products/[id]/page.tsx
@@ -4,6 +4,8 @@ import { useGetProductById } from "@/hooks/product.hook";
 import { useParams } from "next/navigation";
 import ProductDetails from "./components/ProductDetails";
 import LoadingSpinner from "@/components/Shared/LoadingSpinner/LoadingSpinner";
+import Reviews from "./components/Review/Reviews";
+import { Separator } from "@/components/ui/separator";
 
 const ProductDetailsPage = () => {
   const { id } = useParams();
@@ -28,11 +30,18 @@ const ProductDetailsPage = () => {
   const product = productData?.data;
 
   return (
-    <div className="h-full flex-1 flex-col space-y-2 p-8 md:flex">
+    <div className="h-full flex-1 flex-col p-8 md:flex">
       <div className="flex items-center justify-between">
         <h1 className="text-lg font-semibold md:text-2xl">Product Details</h1>
       </div>
       <ProductDetails product={product} />
+
+      <div className="mb-10 mt-32">
+        <Separator />
+        <h2 className="text-xl text-center mt-4">Reviews</h2>
+      </div>
+
+      <Reviews productId={id as string} />
     </div>
   );
 };
